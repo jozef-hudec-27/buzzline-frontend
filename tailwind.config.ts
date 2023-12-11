25bd66886397f8fd7d5160381ddf99de20e088f5
Jozef Hudec <jojohudec27@gmail.com>
2023-12-11 20:20:31 +0100

    Add colors to tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,8 +10,27 @@ const config: Config = {
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      colors: {
+        primary: {
+          DEFAULT: '#C850C0',
+        },
+        secondary: {
+          DEFAULT: '#4158D0',
+        },
+        tertiary: {
+          DEFAULT: '#FFCC70',
+        },
+        black: {
+          100: '#170316',
+          75: '#514250',
+          65: '#685B68',
+          50: '#8B818A',
+          25: '#C5C0C5',
+          10: '#E8E6E8',
+          5: '#F3F2F3',
+        },
       },
     },
   },
