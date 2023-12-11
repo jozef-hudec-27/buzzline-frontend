980cec4387ae8a8a118cac0a0a971f7db95949ad
Jozef Hudec <jojohudec27@gmail.com>
2023-12-11 20:07:56 +0100

    Create axios instance

---
+++ app/api/axiosInstance.ts
@@ -0,0 +1,49 @@
+import axios, { AxiosError } from 'axios'
+
+const baseURL = process.env.BASE_URL
+const axiosOptions = {
+  baseURL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  withCredentials: true,
+}
+
+const axiosInstance = axios.create(axiosOptions)
+
+const api = (sendAuthorization = false) => {
+  if (sendAuthorization) {
+    axiosInstance.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem('accessToken')}`
+  } else {
+    delete axiosInstance.defaults.headers.common['Authorization']
+  }
+
+  return axiosInstance
+}
+
+export default api
+
+// If the access token has expired, issue a new one and retry the request
+axiosInstance.interceptors.response.use(
+  (response) => response,
+
+  async (error: AxiosError) => {
+    const { response, config } = error
+    // If the error is not due to an expired access token, reject the promise
+    // @ts-ignore
+    if (!response?.data?.message?.includes('expired') || !config) {
+      return await Promise.reject(error)
+    }
+
+    try {
+      const response = await axios.post('/auth/refresh', {}, axiosOptions)
+      localStorage.setItem('accessToken', response.data.accessToken)
+      config.headers['Authorization'] = `Bearer ${response.data.accessToken}`
+      return await api(true).request(config) // Retry original request
+    } catch (error2) {
+      localStorage.removeItem('accessToken')
+      window.location.replace('/login?logout=true') // Refresh token has expired, redirect to login
+      return await Promise.reject(error2)
+    }
+  }
+)
