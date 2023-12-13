78ae9778359c095fdd5b372fad7bbc9601184ad3
Jozef Hudec <jojohudec27@gmail.com>
2023-12-13 07:02:35 +0100

    Make header on register page responsive

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -5,11 +5,10 @@ function RegisterPage() {
   return (
     <div>
       <Navbar />
-
-      <section className="mt-[96px] pl-[240px]">
-        <div className="flex items-center gap-[96px]">
+      <section className="mt-[48px] sm:mt-[96px] px-0 sm:px-[24px] xl:px-[240px]">
+        <div className="flex flex-col xl:flex-row items-center gap-[96px]">
           <div className="flex flex-col gap-[32px]">
-            <h1 className="gradient-text text-[90px]">
+            <h1 className="gradient-text text-[60px] sm:text-[90px] leading-none">
               Chat
               <br /> anytime,
               <br /> anywhere
@@ -30,12 +29,12 @@ function RegisterPage() {
             </form>
           </div>
 
-          <div className="p-[24px] flex flex-col gap-[64px] cursor-default w-[59%]">
-            <p className="px-[24px] py-[16px] text-[24px] bg-secondary rounded-[24px] max-w-[640px] text-white shadow-lg self-end">
+          <div className="p-[24px] flex flex-col gap-[32px] sm:gap-[64px] cursor-default w-11/12 sm:w-3/4 xl:w-[59%]">
+            <p className="px-[24px] py-[16px] text-[16px] sm:text-[24px] bg-secondary rounded-[24px] max-w-[640px] text-white shadow-lg self-end">
               Hey there! 👋 I was wondering whether or not you're down to a party tonight? It's gonna be a blast!
             </p>
 
-            <p className="px-[24px] py-[16px] text-[24px] bg-black-10 rounded-[24px] max-w-[640px] shadow-lg">
+            <p className="px-[24px] py-[16px] text-[16px] sm:text-[24px] bg-black-10 rounded-[24px] max-w-[640px] shadow-lg">
               Yo, absolutely! I'm down for whatever. Just let me know when and where.
             </p>
           </div>
