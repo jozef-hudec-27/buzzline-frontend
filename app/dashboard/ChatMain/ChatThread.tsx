f94a33333bb00c55001ad4e5eafa6acfeaec01de
Jozef Hudec <jojohudec27@gmail.com>
2023-12-23 08:51:28 +0100

    Show message createdAt tooltip on hover

--- app/dashboard/ChatMain/ChatThread.tsx
+++ app/dashboard/ChatMain/ChatThread.tsx
@@ -2,10 +2,15 @@
 
 import { useEffect, useRef } from 'react'
 import Image from 'next/image'
+import { Tooltip } from 'react-tooltip'
 
 import useUserStore from '@/app/zustand/userStore'
 
-import { Message } from '@/app/types'
+import { Message, User } from '@/app/types'
+
+function msgBelongsToUser(msg: Message, user: User): boolean {
+  return msg?.sender._id === user._id
+}
 
 type ChatThreadProps = {
   messages: Message[]
@@ -20,11 +25,21 @@ function ChatThread({ messages, messagesLoading }: ChatThreadProps) {
   const threadRef = useRef<HTMLDivElement>(null)
 
   useEffect(() => {
-    // Scroll to bottom when opened or new message
-    if (messages.length) {
+    // Scroll to bottom when chat opened
+    threadRef.current?.scrollTo(0, threadRef.current?.scrollHeight)
+  }, [])
+
+  useEffect(() => {
+    // Scroll to bottom when new message, if already at bottom
+    const messagesEl = threadRef.current
+    if (
+      messages.length &&
+      messagesEl &&
+      messagesEl?.scrollHeight - messagesEl?.scrollTop - messagesEl?.clientHeight < 200
+    ) {
       threadRef.current?.scrollTo(0, threadRef.current?.scrollHeight)
     }
-  }, [messages])
+  }, [messages, threadRef])
 
   const user = useUserStore((state) => state.user)
 
@@ -34,24 +49,41 @@ function ChatThread({ messages, messagesLoading }: ChatThreadProps) {
         {messages.map((msg, i) => {
           return (
             <div
-              key={i}
-              className={`flex items-end gap-[8px] ${msg.sender._id === user._id ? 'self-end' : ''} ${
-                i > 0 && messages[i - 1].sender._id !== msg.sender._id ? 'mt-[60px]' : ''
+              key={msg._id}
+              className={`flex items-end gap-[8px] ${msgBelongsToUser(msg, user) ? 'self-end' : ''} ${
+                i > 0 && !msgBelongsToUser(messages[i - 1], msg.sender) ? 'mt-[60px]' : ''
               }`}
             >
-              {msg.sender._id !== user._id && (
+              {!msgBelongsToUser(msg, user) && (
                 <Image
                   src="https://faceboom.onrender.com/rails/active_storage/blobs/redirect/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBCZz09IiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--e4a44a7bd0b470dc8d229be0daebaa1accc5deea/default-avatar.svg"
                   alt="avatar"
                   width={28}
                   height={28}
                   className={`w-[28px] h-[28px] rounded-full relative top-[4px] ${
-                    messages[i + 1]?.sender._id === msg.sender._id ? 'opacity-0' : ''
+                    msgBelongsToUser(messages[i + 1], msg.sender) ? 'opacity-0' : ''
                   }`}
                 />
               )}
 
-              <div className={`message ${msg.sender._id === user._id ? 'own' : 'other'} `}>{msg.content}</div>
+              <div
+                className={`message ${msgBelongsToUser(msg, user) ? 'own' : 'other'} `}
+                data-tooltip-id={`tooltip-${msg._id}`}
+                data-tooltip-content={new Date(msg.createdAt).toLocaleString('en-GB', {
+                  year: '2-digit',
+                  month: '2-digit',
+                  day: '2-digit',
+                  hour: 'numeric',
+                  minute: '2-digit',
+                  hour12: true,
+                })}
+                data-tooltip-place={msgBelongsToUser(msg, user) ? 'left' : 'right'}
+                data-tooltip-delay-show={400}
+              >
+                {msg.content}
+              </div>
+
+              <Tooltip id={`tooltip-${msg._id}`} className="!rounded-[8px] !bg-black-100 !drop-shadow-md" arrowColor="transparent" offset={4} />
             </div>
           )
         })}
