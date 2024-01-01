1a2d888651f975500e8e96ba6bb415556862650d
Jozef Hudec <jojohudec27@gmail.com>
2024-01-01 12:43:54 +0100

    Prevent logged in users from going to register, login pages

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,13 +1,23 @@
 'use client'
 
 import { useState, useEffect } from 'react'
+import { useRouter } from 'next/navigation'
 import toast from 'react-hot-toast'
+
+import useUserStore from '@/app/zustand/userStore'
 
 import Navbar from '../components/navbar/Navbar'
 import LoginForm from './LoginForm'
+
 import { LoginFormState } from '@/app/types'
 
 function LoginPage() {
+  const router = useRouter()
+  const { isLoggedIn, isLoading } = useUserStore((state) => ({
+    isLoggedIn: state.isLoggedIn,
+    isLoading: state.isLoading,
+  }))
+
   const [formState, setFormState] = useState<LoginFormState>({ email: '', password: '' })
 
   useEffect(() => {
@@ -18,22 +28,30 @@ function LoginPage() {
     }
   }, [])
 
-  return (
-    <div>
-      <Navbar />
+  useEffect(() => {
+    if (isLoggedIn && !isLoading) {
+      router.replace('/')
+    }
+  }, [isLoggedIn, isLoading])
 
-      <section className="mt-[48px] sm:mt-[96px]">
-        <div className="flex flex-col gap-[32px] w-fit mx-auto">
-          <h1 className="gradient-text text-[60px] sm:text-[90px] leading-none">
-            Log in to
-            <br /> BuzzLine
-          </h1>
+  if (!isLoggedIn && !isLoading) {
+    return (
+      <div>
+        <Navbar />
 
-          <LoginForm formState={formState} setFormState={setFormState} />
-        </div>
-      </section>
-    </div>
-  )
+        <section className="mt-[48px] sm:mt-[96px]">
+          <div className="flex flex-col gap-[32px] w-fit mx-auto">
+            <h1 className="gradient-text text-[60px] sm:text-[90px] leading-none">
+              Log in to
+              <br /> BuzzLine
+            </h1>
+
+            <LoginForm formState={formState} setFormState={setFormState} />
+          </div>
+        </section>
+      </div>
+    )
+  }
 }
 
 export default LoginPage
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,6 +1,9 @@
 'use client'
 
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import { useRouter } from 'next/navigation'
+
+import useUserStore from '@/app/zustand/userStore'
 
 import Navbar from '../components/navbar/Navbar'
 import RegisterForm from './RegisterForm'
@@ -8,6 +11,18 @@ import RegisterForm from './RegisterForm'
 import { RegisterFormState } from '@/app/types'
 
 function RegisterPage() {
+  const router = useRouter()
+  const { isLoggedIn, isLoading } = useUserStore((state) => ({
+    isLoggedIn: state.isLoggedIn,
+    isLoading: state.isLoading,
+  }))
+
+  useEffect(() => {
+    if (isLoggedIn && !isLoading) {
+      router.replace('/')
+    }
+  }, [isLoggedIn, isLoading])
+
   const [page, setPage] = useState<0 | 1>(0)
   // 0 -> email, password, confirm password 1 -> first name, last name
   const [formState, setFormState] = useState<RegisterFormState>({
@@ -18,34 +33,36 @@ function RegisterPage() {
     confirmPassword: '',
   })
 
-  return (
-    <div>
-      <Navbar />
-      <section className="mt-[48px] sm:mt-[96px] px-0 sm:px-[24px] xl:px-[240px]">
-        <div className="flex flex-col xl:flex-row items-center gap-[96px]">
-          <div className="flex flex-col gap-[32px] max-w-[322px] sm:max-w-[435px]">
-            <h1 className="gradient-text text-[60px] sm:text-[90px] leading-none">
-              Chat
-              <br /> anytime,
-              <br /> anywhere
-            </h1>
+  if (!isLoggedIn && !isLoading) {
+    return (
+      <div>
+        <Navbar />
+        <section className="mt-[48px] sm:mt-[96px] px-0 sm:px-[24px] xl:px-[240px]">
+          <div className="flex flex-col xl:flex-row items-center gap-[96px]">
+            <div className="flex flex-col gap-[32px] max-w-[322px] sm:max-w-[435px]">
+              <h1 className="gradient-text text-[60px] sm:text-[90px] leading-none">
+                Chat
+                <br /> anytime,
+                <br /> anywhere
+              </h1>
 
-            <RegisterForm page={page} setPage={setPage} formState={formState} setFormState={setFormState} />
+              <RegisterForm page={page} setPage={setPage} formState={formState} setFormState={setFormState} />
+            </div>
+
+            <div className="p-[24px] flex flex-col gap-[32px] sm:gap-[64px] cursor-default w-11/12 sm:w-3/4 xl:w-[59%]">
+              <p className="px-[24px] py-[16px] text-[16px] sm:text-[24px] bg-secondary rounded-[24px] max-w-[640px] text-white shadow-lg self-end">
+                Hey there! 👋 I was wondering whether or not you're down to a party tonight? It's gonna be a blast!
+              </p>
+
+              <p className="px-[24px] py-[16px] text-[16px] sm:text-[24px] bg-black-10 rounded-[24px] max-w-[640px] shadow-lg">
+                Yo, absolutely! I'm down for whatever. Just let me know when and where.
+              </p>
+            </div>
           </div>
-
-          <div className="p-[24px] flex flex-col gap-[32px] sm:gap-[64px] cursor-default w-11/12 sm:w-3/4 xl:w-[59%]">
-            <p className="px-[24px] py-[16px] text-[16px] sm:text-[24px] bg-secondary rounded-[24px] max-w-[640px] text-white shadow-lg self-end">
-              Hey there! 👋 I was wondering whether or not you're down to a party tonight? It's gonna be a blast!
-            </p>
-
-            <p className="px-[24px] py-[16px] text-[16px] sm:text-[24px] bg-black-10 rounded-[24px] max-w-[640px] shadow-lg">
-              Yo, absolutely! I'm down for whatever. Just let me know when and where.
-            </p>
-          </div>
-        </div>
-      </section>
-    </div>
-  )
+        </section>
+      </div>
+    )
+  }
 }
 
 export default RegisterPage
